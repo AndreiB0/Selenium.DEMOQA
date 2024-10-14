03074d5a56d3c714633292e75eec8b35fe266110
Andrei <andrei.bonci@gmail.com>
2024-10-14 18:30:53 +0300

    fixed failing TextBoxTest.java

--- src/main/java/com/demoqa/pages/Elements/TextBoxPage.java
+++ src/main/java/com/demoqa/pages/Elements/TextBoxPage.java
@@ -1,64 +1,71 @@
 package com.demoqa.pages.Elements;
 
-import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 
+import static com.Utilities.ActionsUtility.sendKeys;
+import static com.Utilities.GetUtility.getText;
+import static com.Utilities.JavaScriptUtility.scrollToElementJS;
+import static com.Utilities.WaitUtility.explicitWaitUntilVisible;
 
-import static dev.failsafe.internal.util.Assert.*;
 
 public class TextBoxPage extends ElementsPage{
     /////Text Box fields in Text Box page
     private By fullNameField           = By.id("userName");
     private By emailField              = By.id("userEmail");
-    private By currentAddressField     = By.id("currentAddress");
-    private By permanentAddressField   = By.id("permanentAddress");
+    private By currentAddressField     = By.xpath("//textarea[@id='currentAddress']");;
+    private By permanentAddressField   = By.xpath("//textarea[@id='permanentAddress']");
     private By submitButton            = By.id("submit");
 
     /////Text Box values after pressing Submit
     private By enteredFullName         = By.id("name");
     private By enteredEmail            = By.id("email");
-    private By enteredCurrentAddress   = By.xpath("//p[@id='currentAddress' and @class='mb-1']");
-    private By enteredPermanentAddress = By.xpath("//p[@id='permanentAddress' and @class='mb-1']");
+    private By enteredCurrentAddress   = By.xpath("//p[@id='currentAddress']");
+    private By enteredPermanentAddress = By.xpath("//p[@id='permanentAddress']");
 
 
     ///Set values for Fields in Text Box
     //Set Full name
-    public void setFullName(String fullName){
-        click(fullNameField);
-        set(fullNameField, fullName);
+    public void setFullName(String name){
+        delay(1000);
+        scrollToElementJS(fullNameField);
+        sendKeys(find(fullNameField), Keys.chord(name));
     }
 
     //Set Email
     public void setEmail(String email){
-        click(emailField);
-        set(emailField, email);
+        setFullName(Keys.chord(Keys.TAB, email));
+
     }
 
     //Set Current Address
     public void setCurrentAddress(String currentAddress){
-        click(currentAddressField);
-        set(currentAddressField, currentAddress);
+        find(currentAddressField).sendKeys(currentAddress + Keys.ENTER);
+
     }
 
     //Set Permanent Address
     public void setPermanentAddress(String permanentAddress){
-        click(permanentAddressField);
-        set(permanentAddressField, permanentAddress);
+        find(permanentAddressField).sendKeys(permanentAddress + Keys.ENTER);
     }
 
     public void clickSubmit(){
+        scrollToElementJS(submitButton);
         click(submitButton);
     }
 
     //Get values after submit
     //Get Full Name
     public String getFullName(){
-        return find(enteredFullName).getText();
+        explicitWaitUntilVisible(5, enteredFullName);
+        return getText(enteredFullName);
     }
 
     //Get Email
     public String getEmail(){
-        return find(enteredEmail).getText();
+        explicitWaitUntilVisible(5, enteredEmail);
+        return getText(enteredEmail);
     }
 
     //Get Current Address
--- src/test/java/com/demoqa/tests/ElementsPageTests/TextBoxTest.java
+++ src/test/java/com/demoqa/tests/ElementsPageTests/TextBoxTest.java
@@ -6,37 +6,34 @@ import org.testng.annotations.Test;
 
 public class TextBoxTest extends BaseTest {
     @Test
-    public void testFullName(){
+    public void testTextBox(){
         String fullName = "Andrei Bonci";
         String email = "andrei.bonci@kek.com";
-        String currentAddress = "San Francisco";
+        String currentAddressCity = "1234 San Francisco";
         String permanentAddress = "New York";
 
-        var textFields = homePage.goToElements().clickTextBox();
-        textFields.setFullName(fullName);
-        textFields.setEmail(email);
-        textFields.setCurrentAddress(currentAddress);
-        textFields.setPermanentAddress(permanentAddress);
-        textFields.clickSubmit();
+        var textBoxPage = homePage.goToElements().clickTextBox();
+        textBoxPage.setFullName(fullName);
+        textBoxPage.setEmail(email);
+        textBoxPage.setCurrentAddress(currentAddressCity);
+        textBoxPage.setPermanentAddress(permanentAddress);
+        textBoxPage.clickSubmit();
 
         //Assertion for Full Name
-        String expectedFullName = textFields.getFullName();
+        String expectedFullName = textBoxPage.getFullName();
         Assert.assertEquals("Name:" + fullName, expectedFullName);
 
-        //Assertion for Email
-        String expectedEmail = textFields.getEmail();
-        Assert.assertEquals("Email:"+email, expectedEmail);
+        //Assertion for email
+        String expectedEmail = textBoxPage.getEmail();
+        Assert.assertEquals("Email:" + email, expectedEmail);
 
         //Assertion for Current Address
-        String expectedCurrentAddress = textFields.getCurrentAddress();
-        Assert.assertNotEquals("Permananet Address : "+ currentAddress, expectedCurrentAddress);
-
+        String expectedCurrentAddress = textBoxPage.getCurrentAddress();
+        Assert.assertNotEquals("Permananet Address : "+ currentAddressCity, expectedCurrentAddress);
 
         //Assertion for Permanent Address
-        String expectedPermanentAddress = textFields.getPermanentAddress();
+        String expectedPermanentAddress = textBoxPage.getPermanentAddress();
         Assert.assertNotEquals("Permananet Address : "+ permanentAddress, expectedPermanentAddress);
-
-
     }
 }
 
